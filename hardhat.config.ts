0948143aa625a624b9b8abd08b0030938ec4f805
frankdev7 <franklyncr12@gmail.com>
2023-03-31 09:56:30 -0500

    first commit

---
+++ hardhat.config.ts
@@ -0,0 +1,27 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+// Go to https://alchemy.com, sign up, create a new App in
+// its dashboard, and replace "KEY" with its key
+const ALCHEMY_API_KEY = process.env.ALCHEMY_API_KEY;
+
+// Replace this private key with your Sepolia account private key
+// To export your private key from Metamask, open Metamask and
+// go to Account Details > Export Private Key
+// Beware: NEVER put real Ether into testing accounts
+const SEPOLIA_PRIVATE_KEY: string = process.env.SEPOLIA_PRIVATE_KEY;
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.1",
+  networks: {
+    sepolia: {
+      url: `https://eth-sepolia.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
+      accounts: [SEPOLIA_PRIVATE_KEY]
+    }
+  }
+};
+
+export default config;
